1d19c1008171846431bdad2c6e1b04cc568653c2
Mouafak Dakhlaoui <57966089+mouaff25@users.noreply.github.com>
2023-09-21 14:57:27 +0100

    feat: add script for running API
    * feat: add main script for running api
    
    * docs: add version file
    
    * docs: add documentation for the endpoints
    
    * refactor: remove old test file
    
    * refactor: update settings.py
    
    * refactor: use app settings in api parameters
    
    * refactor: remove old settings from settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,6 +1,7 @@
 from functools import lru_cache
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import List, Optional
 
 
 class Settings(BaseSettings):
@@ -9,21 +10,17 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     # App settings
-    APP_NAME: str = "My App"
-    APP_DESCRIPTION: str = "My App Description"
-    APP_VERSION: str = "0.0.1"
     APP_HOST: str = "localhost"
     APP_PORT: int = 8000
-    APP_DOCS_URL: str = "/docs"
-    APP_REDOC_URL: str = "/redoc"
-    APP_OPENAPI_URL: str = "/openapi.json"
+    APP_DOCS_URL: Optional[str] = "/docs"
+    APP_REDOC_URL: Optional[str] = "/redoc"
+    APP_OPENAPI_URL: Optional[str] = "/openapi.json"
     APP_DEBUG: bool = False
 
     # llm settings
     STORY_DESIGNER_MODEL_FILENAME: str = "llama-2-13b-chat.ggmlv3.q3_K_L.bin"
-    LLM_MODEL_DIR: str = "./llms"
     STORY_DESIGNER_REPO_ID: str = "TheBloke/Llama-2-13B-chat-GGML"
-    STORY_DESIGNER_STOP: list = []
+    STORY_DESIGNER_STOP: List[str] = [""]
     STORY_DESIGNER_TEMPERATURE: float = 0
     STORY_DESIGNER_REPEAT_PENALTY: float = 1.1
     STORY_DESIGNER_VERBOSE: bool = False
@@ -38,16 +35,6 @@ class Settings(BaseSettings):
     STORY_DESIGNER_CHARACTERS_DESIGNER_PROMPT_PATH: str = "./app/prompts/llama2-characters-designer.yaml"
     STORY_DESIGNER_INTRODUCTION_PLANNER_PROMPT_PATH: str = "./app/prompts/llama2-introduction-planner.yaml"
     STORY_DESIGNER_INTRODUCTION_WRITER_PROMPT_PATH: str = "./app/prompts/llama2-introduction-writer.yaml"
-    STORY_WRITER_REPO_ID: str = "TheBloke/MPT-7B-Storywriter-GGML"
-    STORY_WRITER_MODEL_FILENAME: str = "mpt-7b-storywriter.ggmlv3.fp16.bin"
-    STORY_WRITER_MAX_NEW_TOKENS: int = 65000
-    STORY_WRITER_GPU_LAYERS: int = 50
-    STORY_WRITER_BATCH_SIZE: int = 128
-    STORY_WRITER_REPETITION_PENALTY: float = 1.1
-    STORY_WRITER_TEMPERATURE: float = 0.5
-    STORY_WRITER_STOP: list = ["\n\n\n\n\n"]
-    STORY_WRITER_VERBOSE: bool = False
-
 
 @lru_cache()
 def get_settings() -> Settings:
