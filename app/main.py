72364860558dcf0867f08a744937039d0e550a67
Mouafak Dakhlaoui <mouaffak9@gmail.com>
2023-09-21 14:08:40 +0100

    docs: add documentation for the endpoints

--- app/main.py
+++ app/main.py
@@ -1,30 +1,65 @@
 from app.story_designer import LlamaStoryTeller
 from fastapi import FastAPI
 import uvicorn
+from app import __version__
 
 story_teller = LlamaStoryTeller()
 
 story_teller.load_llm()
 
-app = FastAPI()
+app = FastAPI(
+    title="AI Story Teller",
+    description="An AI that can help you write a story",
+    version=__version__,
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
 
-@app.post("/brainstorming")
+@app.post(
+        "/brainstorming",
+        summary="Brainstorming",
+        description="""This endpoint is used to brainstorm ideas for a story.
+
+The input text should be a prompt that describes the story topic. The output is a list of 5 creative ideas for the story.""",
+    )
 async def brainstorming(prompt: str):
     return story_teller.brainstorm(prompt)
 
-@app.post("/idea_choice")
+
+@app.post(
+        "/idea_choice",
+        summary="Idea Choice",
+        description="""This endpoint is used to choose one of the ideas for a story.
+        
+The input text should be a prompt that describes the story topic and the list of ideas. The output is an idea choice.""",)
 async def idea_choice(story_topic: str, ideas: str):
     return story_teller.choose_idea(story_topic=story_topic, ideas=ideas)
 
-@app.post("/events_outline")
+@app.post(
+        "/events_outline",
+
+        summary="Events Outline",
+        description="""This endpoint is used to outline the sequence of events for a story.
+        
+The input text should be a prompt that describes the story topic and the idea. The output is a sequence of events.""",)
 async def events_outline(story_topic: str, story_idea: str):
     return story_teller.outline_sequence_of_events(story_topic=story_topic, idea=story_idea)
 
-@app.post("/characters_designer")
+@app.post(
+        "/characters_designer",
+        summary="Characters Designer",
+        description="""This endpoint is used to design the characters for a story.
+        
+        The input text should be a prompt that describes the story topic and the sequence of events. The output is a list of characters with a detailed description of their appearance, personality and relationships with each other.""",)
 async def characters_designer(story_topic: str, events_outline: str):
     return story_teller.design_characters(story_topic=story_topic, events_outline=events_outline)
 
-@app.post("/introduction_planner")
+@app.post(
+        "/introduction_planner",
+        summary="Introduction Planner",
+        description="""This endpoint is used to plan the introduction for a story.
+        
+        The input text should be a prompt that describes the sequence of events. The output is a plan for the introduction.""",)
 async def introduction_planner(events_outline: str):
     return story_teller.plan_introduction(events_outline=events_outline)
 
