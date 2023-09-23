45b27ddf652bd23e0d749d405ed219216f6ee67e
Mouafak Dakhlaoui <mouaffak9@gmail.com>
2023-09-23 12:04:53 +0100

    fix: fix default settings

--- app/config/settings.py
+++ app/config/settings.py
@@ -18,15 +18,15 @@ class Settings(BaseSettings):
     APP_DEBUG: bool = False
 
     # llm settings
-    STORY_DESIGNER_MODEL_FILENAME: str = "llama-2-13b-chat.ggmlv3.q3_K_L.bin"
-    STORY_DESIGNER_REPO_ID: str = "TheBloke/Llama-2-13B-chat-GGML"
-    STORY_DESIGNER_STOP: List[str] = [""]
+    STORY_DESIGNER_MODEL_FILENAME: str = "llama-2-7b-chat.ggmlv3.q2_K.bin"
+    STORY_DESIGNER_REPO_ID: str = "TheBloke/Llama-2-7B-Chat-GGML"
+    STORY_DESIGNER_STOP: List[str] = ["\n###"]
     STORY_DESIGNER_TEMPERATURE: float = 0
     STORY_DESIGNER_REPEAT_PENALTY: float = 1.1
     STORY_DESIGNER_VERBOSE: bool = False
     STORY_DESIGNER_N_CTX: int = 4096
-    STORY_DESIGNER_N_GPU_LAYERS: int = 50
-    STORY_DESIGNER_N_BATCH: int = 512
+    STORY_DESIGNER_N_GPU_LAYERS: int = 20
+    STORY_DESIGNER_N_BATCH: int = 128
     STORY_DESIGNER_MAX_TOKENS: int = 2048
     STORY_DESIGNER_PROMPT_PATH: str = "./app/prompts/llama2.yaml"
     STORY_DESIGNER_BRAINSTORMING_PROMPT_PATH: str = "./app/prompts/llama2-brainstorming.yaml"
